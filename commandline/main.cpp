89b7687cc63811568b030026c11ff984cbb164e8
Brian Shewan <brian.shewan@nscc.ca>
2022-01-13 09:33:11 -0800

    added commandline example

---
+++ commandline/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    for (int i = 0; i < argc; ++i) {
+        cout << argv[i] << endl;
+    }
+
+    if (argc > 1) {
+        cout << "You want to edit: " << argv[1] << endl;
+    } else {
+        cout << "You need to add a filename." << endl;
+    }
+
+    return 0;
+}
